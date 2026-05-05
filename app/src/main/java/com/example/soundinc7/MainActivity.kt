7419bb46885f6d613bab52923fa9385e943f85b1
Eduardo Becerra <josebecerra.0870@gmail.com>
2026-05-04 19:03:19 -0700

    Assignment 2 viewmodel connected to loginScreen

--- app/src/main/java/com/example/soundinc7/MainActivity.kt
+++ app/src/main/java/com/example/soundinc7/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soundinc7.ui.screens.LoginContent
+import com.example.soundinc7.ui.screens.LoginScreen
 import com.example.soundinc7.ui.theme.SoundInC7Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +28,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SoundInC7Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen(
+                        onNavigateToRegister = {}
                     )
                 }
             }
@@ -30,18 +41,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SoundInC7Theme {
-        Greeting("Android")
+fun SoundInPreview(){
+    SoundInC7Theme() {
+        LoginScreen(
+            onNavigateToRegister = {}
+        )
     }
 }
---
+++ app/src/main/java/com/example/soundinc7/ui/screens/RegisterScreen.kt
@@ -0,0 +1,2 @@
+package com.example.soundinc7.ui.screens
+
